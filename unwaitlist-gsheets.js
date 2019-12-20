98ea9b3b207adf5277a0d268fd07822ca2817aac
James <jamestedesco802@gmail.com>
2019-12-19 22:51:24 -0500

    Added nodemailer, dotenv, and program can now evaluate spreadsheet status and email accordingly

--- unwaitlist-gsheets.js
+++ unwaitlist-gsheets.js
@@ -1,9 +1,11 @@
-// npm install googleapis@39 --save
+// npm install googleapis@39 --save nodemailer dotenv
 
 
 // require dependencies
 const GoogleSpreadsheet = require('google-spreadsheet');
 const { promisify } = require('util');
+const nodemailer = require('nodemailer');
+require('dotenv').config()
 
 // call credentials
 const creds = require('./client_secret.json')
@@ -29,27 +31,52 @@ async function accessSpreadsheet() {
     });
 
     // declare cells
-    const cells = await promisify(sheet.getCells)({  
-    })
+    // const cells = await promisify(sheet.getCells)({  
+    // })
 
 
 
     // iterate through every row
     rows.forEach(row => {
         // if email sent, do nothing
-        if(row.initialemail == "Confirmation Sent") {
-            console.log("Already sent --> ", row.email)
-        } 
-        // else, email user to confirm their registration
-        else {
+        if(row.initialemail != "Confirmation Sent") {
             // TODO: integrate with nodemailer for confirmation emails
 
+            // begin working with nodemailer
+            let transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    user: process.env.EMAIL_USER,
+                    pass: process.env.EMAIL_PASS
+                }
+            });
+            
+            // set email content
+            let mailOptions = {
+                from: 'unwaitlist.io@gmail.com',
+                // set useremail
+                to: row.email,
+                subject: 'Unwaitlist Confirmation',
+                text: `Unwaitlist is now checking your course: https://www.uvm.edu/academics/courses/?term=202001&crn=${row.courseregistrationnumber}`
+            };
+
+            transporter.sendMail(mailOptions, function (error, info) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log('Email sent: ' + info.response);
+                }
+            });
 
 
             // after sent, write confirmation to spreadsheet
             row.initialemail = "Confirmation Sent"
             row.save()
             console.log("Sending now --> ", row.email)
+        } 
+        // else, email user to confirm their registration
+        else {
+            console.log("Already sent")
         }
     })
 
