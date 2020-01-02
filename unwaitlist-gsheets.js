03af695e7ed6a8dd8bc60ef354979273dc6d7bf4
James <jamestedesco802@gmail.com>
2020-01-02 12:01:10 -0500

    Breaking program into unique functions, but still isn't functioning

--- unwaitlist-gsheets.js
+++ unwaitlist-gsheets.js
@@ -13,6 +13,52 @@ const nodemailer = require('nodemailer');
 // require environment variables
 require('dotenv').config()
 
+
+// send confirmation info
+function evaluateRequest(rowsOfRequestSheet, rowsOfCancelationSheet, rowsOfStaticCourseInfo) {
+
+
+    // iterate through every row of main request sheet
+    rowsOfRequestSheet.forEach(row => {
+
+        // first, make sure this is a new user by checking they haven't been confirmed with but the line isn't blank
+        if (row.courseregistrationnumber != "" && row.initialemail != "Confirmation Sent") {
+
+            console.log("\nEntered new section: missing confirmation")
+
+            // indicate if duplicate
+            console.log("Testing for duplicate")
+            let duplicateStatus = checkIfDuplicate(row, rowsOfRequestSheet)
+            // trying to extract true/false value from called function
+            console.log(duplicateStatus)
+
+            // create variable outside of if-statement
+            let validityOfCRN = true
+
+            if (duplicateStatus === false) {
+                console.log("failed duplicate test")
+                // ensure CRN exists for current semester, and email user if unfound
+                validityOfCRN = checkCRNValidity(row, rowsOfStaticCourseInfo)
+
+                //trying to evaluate validity as false here and pass that out of this if-statement
+                console.log("validity logged as false", validityOfCRN)
+
+            } else if (validityOfCRN === false) {
+                // this seems like it should be part of the main program
+                // if email and crn from cancelation sheet match (and not already handled), mark cancelation on request sheet
+                checkIfCanceled(row, rowsOfCancelationSheet)
+
+            } else {
+                // if valid request, send confirmation to student
+                confirmedRequest(row)
+
+            }
+
+        }
+    })
+
+}
+
 // this function is in a loop checking all combinations of requests and cancelations
 function checkIfCanceled(row, rowsOfCancelationSheet) {
     rowsOfCancelationSheet.forEach(canceledRow => {
@@ -39,90 +85,65 @@ function checkIfCanceled(row, rowsOfCancelationSheet) {
     })
 }
 
-function checkCRNValidity(row, rowsOfStaticCourseInfo) {
-    rowsOfStaticCourseInfo.forEach(infoRow => {
-        if (infoRow.compnumb != row.courseregistrationnumber) {
-            row.currentstatus = "Unfound CRN"
-            row.save()
-            // send unfound email
-            console.log("Unfound CRN")
-        }
-    })
-}
-
-function unfoundCRN(currentRow, rowsOfRequestSheet) {
+function checkIfDuplicate(currentRequestRow, rowsOfRequestSheet) {
     rowsOfRequestSheet.forEach(row => {
-        if (currentRow == row) {
+        if (currentRequestRow == row) {
             // send duplicate email
             console.log("Duplicate")
+            let confirmedDuplicate = true
+            return confirmedDuplicate
         }
     })
 }
 
-
-// send confirmation info
-function evaluateRequest(rowsOfRequestSheet, rowsOfCancelationSheet, rowsOfStaticCourseInfo) {
-
-    // iterate through every row of main request sheet
-    rowsOfRequestSheet.forEach(row => {
-
-        // first, make sure this is a new user by checking they haven't been confirmed with but the line isn't blank
-        if (row.courseregistrationnumber != "" && row.initialemail != "Confirmation Sent") {
-
-            // if email and crn from cancelation sheet match (and not already handled), mark cancelation on request sheet
-            checkIfCanceled(row, rowsOfCancelationSheet)
-
-        //roll these into a single if statement with elifs for other conditions
-            // ensure CRN exists for current semester, and email user if unfound
-            checkCRNValidity(row, rowsOfStaticCourseInfo)
-            // indicate if duplicate
-            unfoundCRN(row, rowsOfRequestSheet)
-            // send email if confirming request enrollment
-            // confirmedRequest()
-
-            // if neither marked canceled nor confirmed, send confirmation email
-            if (row.currentstatus != "Canceled" && row.currentstatus != "Unfound CRN") {
-                // console.log() which student is currently being reviewed
-                console.log("Available Class:", row.courseregistrationnumber, row.email)
-
-                // begin working with nodemailer
-                let transporter = nodemailer.createTransport({
-                    service: 'gmail',
-                    auth: {
-                        user: process.env.EMAIL_USER,
-                        pass: process.env.EMAIL_PASS
-                    }
-                });
-                // declare email content
-                let mailOptions = {
-                    from: 'unwaitlist.io@gmail.com',
-                    // set user email
-                    to: row.email,
-                    subject: 'Unwaitlist Confirmation',
-                    text: `Unwaitlist is now checking your course: https://www.uvm.edu/academics/courses/?term=202001&crn=${row.courseregistrationnumber}`
-                };
-                // send email
-                transporter.sendMail(mailOptions, function (error, info) {
-                    if (error) {
-                        console.log(error);
-                    } else {
-                        console.log(`Email sent to ${row.email} --> ` + info.response);
-                    }
-                });
-
-                // after sent, write confirmation to spreadsheet
-                row.initialemail = "Confirmation Sent"
-                row.currentstatus = "Watching"
-                row.save()
-            }
-
-            // add to confirmations indexing
-            if (row.currentstatus == "Watching") {
-                watchingList.push(row.email)
-            }
+function checkCRNValidity(requestRow, rowsOfStaticCourseInfo) {
+    rowsOfStaticCourseInfo.forEach(infoRow => {
+        if (infoRow.compnumb != requestRow.courseregistrationnumber) {
+            requestRow.currentstatus = "Unfound CRN"
+            requestRow.save()
+            // send unfound email
+            console.log("Unfound CRN")
+            return false
         }
     })
+}
 
+function confirmedRequest(row) {
+    // if neither marked canceled nor confirmed, send confirmation email
+    if (row.currentstatus != "Canceled" && row.currentstatus != "Unfound CRN") {
+        // console.log() which student is currently being reviewed
+        console.log("Available Class:", row.courseregistrationnumber, row.email)
+
+        // begin working with nodemailer
+        let transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS
+            }
+        });
+        // declare email content
+        let mailOptions = {
+            from: 'unwaitlist.io@gmail.com',
+            // set user email
+            to: row.email,
+            subject: 'Unwaitlist Confirmation',
+            text: `Unwaitlist is now checking your course: https://www.uvm.edu/academics/courses/?term=202001&crn=${row.courseregistrationnumber}`
+        };
+        // send email
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log(`Email sent to ${row.email} --> ` + info.response);
+            }
+        });
+
+        // after sent, write confirmation to spreadsheet
+        row.initialemail = "Confirmation Sent"
+        row.currentstatus = "Watching"
+        row.save()
+    }
 }
 
 
@@ -135,6 +156,8 @@ async function accessSpreadsheet() {
     // load spreadsheets
     const requestSheet = info.worksheets[0];
     const cancelationSheet = info.worksheets[1];
+
+    // replace with local storage
     const staticCourseInfoSheet = info.worksheets[2]
     // console.log() show which sheets are loaded
     console.log(`\nLoaded Spreadsheets: "${requestSheet.title}" and "${cancelationSheet.title}" and "${staticCourseInfoSheet.title}" `);
@@ -144,9 +167,12 @@ async function accessSpreadsheet() {
     });
     const rowsOfCancelationSheet = await promisify(cancelationSheet.getRows)({
     })
+
+    // replace this with locally stored data
     const rowsOfStaticCourseInfo = await promisify(staticCourseInfoSheet.getRows)({
     })
 
+    console.log("connected")
     evaluateRequest(rowsOfRequestSheet, rowsOfCancelationSheet, rowsOfStaticCourseInfo)
 
 
